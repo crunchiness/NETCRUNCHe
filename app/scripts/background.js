145642b5159f20e0684c85b7e9a31fe1f70ee537
Ingvaras Merkys <ingvaras@gmail.com>
2015-02-07 16:17:15 +0000

    data saved in local storage

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,54 +1,104 @@
 'use strict';
 
-var statistics = {
-  websites: {},
-  size: 0
-};
+/* Request-Response pairs
+ *
+ * objects:
+ *   globalList: {},
+ *   websiteList: {},
+ *
+ * functions:
+ *   addRequest: addRequest,
+ *   addResponse: addResponse
+ */
+var pairs;
+/* Statistics
+ *
+ * objects:
+ *   websites: {}
+ *
+ * vars:
+ *   size
+ *   inStoreK
+ */
+var statistics;
 
-var pairs = {
-  globalList : {},
-  websiteList : {},
-  addRequest : function (domain, request) {
-    var id = request.requestId;
-    if (id in pairs.globalList) {
-      pairs.globalList[id]['request'] = request;
-    } else {
-      pairs.globalList[id] = {
-        response: null,
-        request: request
-      };
-    }
-    if (!(domain in pairs.websiteList)) {
-      pairs.websiteList[domain] = {domain: domain, pairs: [pairs.globalList[id]]};
-    } else {
-      pairs.websiteList[domain].pairs.push(pairs.globalList[id]);
-    }
-  },
-  addResponse : function (response) {
-    var id = response.requestId;
-    if (id in pairs.globalList) {
-      pairs.globalList[id]['response'] = response;
-    } else {
-      pairs.globalList[id] = {
-        response: response,
-        request: null
-      };
+var writeEvery = 200;
+
+function addRequest(domain, request) {
+  var id = request.requestId;
+  if (id in pairs.globalList) {
+    pairs.globalList[id]['request'] = request;
+  } else {
+    pairs.globalList[id] = {
+      response: null,
+      request: request
+    };
+  }
+  if (!(domain in pairs.websiteList)) {
+    pairs.websiteList[domain] = {domain: domain, pairs: [pairs.globalList[id]]};
+  } else {
+    pairs.websiteList[domain].pairs.push(pairs.globalList[id]);
+  }
+}
+
+function addResponse(response) {
+  var id = response.requestId;
+  if (id in pairs.globalList) {
+    pairs.globalList[id]['response'] = response;
+  } else {
+    pairs.globalList[id] = {
+      response: response,
+      request: null
+    };
+  }
+}
+
+function enrichPairs(pairs) {
+  if (pairs === undefined) pairs = {};
+  return {
+    globalList: {},
+    websiteList: ('websiteList' in pairs) ? pairs.websiteList : {},
+    addRequest: addRequest,
+    addResponse: addResponse
+  };
+}
+
+function preparePairsStorage(pairs) {
+  return {
+    websiteList: pairs.websiteList
+  }
+}
+
+chrome.storage.local.get(null, function (data) {
+  if (('pairs' in data) && ('statistics' in data)) {
+    pairs = enrichPairs(data.pairs);
+    statistics = data.statistics;
+  } else {
+    pairs = enrichPairs();
+    statistics = {
+      websites: {},
+      size: 0,
+      inStoreK: 0
     }
   }
-};
+});
 
 chrome.browserAction.onClicked.addListener(function (activeTab) {
   var newURL = 'main.html';
-  chrome.tabs.create({ url: newURL });
+  chrome.tabs.create({url: newURL});
 });
 
-chrome.runtime.onConnect.addListener(function(port) {
+chrome.runtime.onConnect.addListener(function (port) {
   console.assert(port.name === 'mainToBackground');
-  port.onMessage.addListener(function(msg) {
+  port.onMessage.addListener(function (msg) {
     if (msg.get === 'statistics') {
       port.postMessage({statistics: statistics});
     } else if (msg.get === 'pairs') {
       port.postMessage({pairs: pairs.websiteList});
+    } else if (msg.get === 'space') {
+      chrome.storage.local.getBytesInUse(function(bytes) {
+        port.postMessage({spaceBytes: bytes});
+      })
     }
   });
 });
@@ -58,8 +108,25 @@ function simpleDomain(url) {
   return matches[1];
 }
 
+function checkWrite() {
+  if (((statistics.size - (statistics.size % writeEvery)) / writeEvery) > statistics.inStoreK) {
+    writeToStorage();
+  }
+}
+
+function writeToStorage() {
+  statistics.inStoreK = (statistics.size - (statistics.size % writeEvery)) / writeEvery;
+  chrome.storage.local.set({
+    pairs: preparePairsStorage(pairs),
+    statistics: statistics
+  }, function (data) {
+    console.log('Successfully written to storage.');
+  });
+}
+
 chrome.webRequest.onSendHeaders.addListener(
   function (details) {
+    if (details.url.substr(0, 6) === 'chrome') return;
     var id = details.tabId;
     if (id === -1) {
       // Chrome system tabs usually have this id
@@ -75,6 +142,7 @@ chrome.webRequest.onSendHeaders.addListener(
         statistics.websites[domain] = { domain: domain, size: 1 };
       }
       pairs.addRequest(domain, details);
+      checkWrite()
     });
   },
   {
@@ -85,7 +153,9 @@ chrome.webRequest.onSendHeaders.addListener(
 
 chrome.webRequest.onCompleted.addListener(
   function (details) {
+    if (details.url.substr(0, 6) === 'chrome') return;
     pairs.addResponse(details);
+    statistics.size += 1;
   },
   {urls: ['<all_urls>']},
   ['responseHeaders']);
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,21 +1,43 @@
 'use strict';
 
-angular.module('mainApp', [])
-  .controller('mainCtrl', function ($scope, $timeout) {
-
+var app = angular.module('mainApp', [])
+  .config([
+    '$compileProvider',
+    function ($compileProvider) {
+      $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|blob|chrome-extension):/);
+    }
+  ])
+  .controller('mainCtrl', function ($scope, $timeout, $window) {
+    $scope.url = '';
     var port = chrome.runtime.connect({name: 'mainToBackground'});
     (function tick() {
       port.postMessage({get: 'statistics'});
       $timeout(tick, 1000);
     })();
     port.postMessage({get: 'pairs'});
+    port.postMessage({get: 'space'});
     port.onMessage.addListener(function (msg) {
       if (msg.statistics) {
         $scope.statistics = msg.statistics;
       } else if (msg.pairs) {
         $scope.pairs = JSON.stringify(msg.pairs);
+        var blob = new Blob([$scope.pairs], {type: 'application/json'});
+        $scope.url = $window.URL.createObjectURL(blob);
+        //$scope.url = (window.URL || window.webkitURL).createObjectURL(blob);
+      } else if (msg.spaceBytes) {
+        $scope.space = msg.spaceBytes;
       }
       $scope.$digest();
     });
     $scope.statistics = {};
   });
+
+app.filter('bytes', function () {
+  return function (bytes, precision) {
+    if (isNaN(parseFloat(bytes)) || !isFinite(bytes)) return '-';
+    if (typeof precision === 'undefined') precision = 1;
+    var units = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB'],
+      number = Math.floor(Math.log(bytes) / Math.log(1024));
+    return (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision) + ' ' + units[number];
+  }
+});
